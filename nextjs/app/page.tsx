58eea4f11f9972ee3a862eec9091c90fb04e5e61
sebastian.vangrieken <sebastian.vangrieken12@gmail.com>
2026-02-13 10:52:57 +0100

    loading page testing

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-    return <h1>Hello, Next.js</h1>
+export default async function Page() {
+    const test = await new Promise((res, rej) => {
+        setTimeout(res, 1000);
+    });
+
+    return (
+        <>
+            <h1>Hello, Next.js</h1>
+        </>
+    )
 }
